c154fc2bfc10bd4858ceb044834eadefe8fd4575
Lanuer <play90043@gmail.com>
2021-04-12 18:05:36 +0800

    5 Deploy

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app=Flask(__name__) # __name__ 代表目前執行的模組
 
 @app.route("/") # 函式的裝飾 (Decorator): 已函式為基礎，提供附加的功能
 def home():
-    return 'Hello Flask 1'
+    return 'Hello Flask 2'
 
 @app.route('/test') # 代表我們要處理的網站路徑
 def test():
